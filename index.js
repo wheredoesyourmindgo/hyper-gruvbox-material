7d39f0bc3a6a8c9dd1a4e0e5a444605e9ebf47fb
wheredoesyourmindgo <moemanchu@icloud.com>
2020-03-26 12:32:14 -0700

    Use rollup to produce single index.js.

---
+++ index.js
@@ -0,0 +1,392 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var gruvboxRedColor = "#cc241d";
+var gruvboxOrangeColor = "#d65d0e";
+var gruvboxYellowColor = "#d79921";
+var gruvboxGreenColor = "#98971a";
+var gruvboxAquaColor = "#689d6a";
+var gruvboxBlueColor = "#458588";
+var gruvboxPurpleColor = "#b16286";
+var gruvboxGreyColor = "#928374";
+var gruvboxLightRedColor = "#fb4934";
+var gruvboxLightGreenColor = "#b8bb26";
+var gruvboxLightYellowColor = "#fabd2f";
+var gruvboxLightBlueColor = "#83a598";
+var gruvboxLightPurpleColor = "#d3869b";
+var gruvboxLightAquaColor = "#8ec07c";
+var gruvboxLightGreyColor = "#a89984";
+// const gruvboxLightOrangeColor = '#fe8019'
+var gruvboxDarkRedColor = "#9d0006";
+var gruvboxDarkGreenColor = "#79740e";
+var gruvboxDarkYellowColor = "#b57614";
+var gruvboxDarkBlueColor = "#076678";
+var gruvboxDarkPurpleColor = "#8f3f71";
+var gruvboxDarkAquaColor = "#427b58";
+var gruvboxDarkGreyColor = "#7c6f64";
+// const gruvboxDarkOrangeColor = '#af3a03'
+var gruvboxDarkColorSoftContrast = "#32302f";
+var gruvboxDarkColor = "#282828";
+var gruvboxDarkColorHardContrast = "#1d2021";
+var gruvboxDarkColorOption1 = "#3c3836";
+var gruvboxDarkColorOption2 = "#504945";
+var gruvboxDarkColorOption3 = "#665c54";
+var gruvboxDarkColorOption4 = "#7c6f64";
+var gruvboxLightColorSoftContrast = "#f2e5bc";
+var gruvboxLightColor = "#fbf1c7";
+var gruvboxLightColorHardContrast = "#f9f5d7";
+var gruvboxLightColorOption1 = "#ebdbb2";
+var gruvboxLightColorOption2 = "#d5c4a1";
+var gruvboxLightColorOption3 = "#bdae93";
+var gruvboxLightColorOption4 = "#a89984";
+
+var origPalette = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    gruvboxRedColor: gruvboxRedColor,
+    gruvboxOrangeColor: gruvboxOrangeColor,
+    gruvboxYellowColor: gruvboxYellowColor,
+    gruvboxGreenColor: gruvboxGreenColor,
+    gruvboxAquaColor: gruvboxAquaColor,
+    gruvboxBlueColor: gruvboxBlueColor,
+    gruvboxPurpleColor: gruvboxPurpleColor,
+    gruvboxGreyColor: gruvboxGreyColor,
+    gruvboxLightRedColor: gruvboxLightRedColor,
+    gruvboxLightGreenColor: gruvboxLightGreenColor,
+    gruvboxLightYellowColor: gruvboxLightYellowColor,
+    gruvboxLightBlueColor: gruvboxLightBlueColor,
+    gruvboxLightPurpleColor: gruvboxLightPurpleColor,
+    gruvboxLightAquaColor: gruvboxLightAquaColor,
+    gruvboxLightGreyColor: gruvboxLightGreyColor,
+    gruvboxDarkRedColor: gruvboxDarkRedColor,
+    gruvboxDarkGreenColor: gruvboxDarkGreenColor,
+    gruvboxDarkYellowColor: gruvboxDarkYellowColor,
+    gruvboxDarkBlueColor: gruvboxDarkBlueColor,
+    gruvboxDarkPurpleColor: gruvboxDarkPurpleColor,
+    gruvboxDarkAquaColor: gruvboxDarkAquaColor,
+    gruvboxDarkGreyColor: gruvboxDarkGreyColor,
+    gruvboxDarkColorSoftContrast: gruvboxDarkColorSoftContrast,
+    gruvboxDarkColor: gruvboxDarkColor,
+    gruvboxDarkColorHardContrast: gruvboxDarkColorHardContrast,
+    gruvboxDarkColorOption1: gruvboxDarkColorOption1,
+    gruvboxDarkColorOption2: gruvboxDarkColorOption2,
+    gruvboxDarkColorOption3: gruvboxDarkColorOption3,
+    gruvboxDarkColorOption4: gruvboxDarkColorOption4,
+    gruvboxLightColorSoftContrast: gruvboxLightColorSoftContrast,
+    gruvboxLightColor: gruvboxLightColor,
+    gruvboxLightColorHardContrast: gruvboxLightColorHardContrast,
+    gruvboxLightColorOption1: gruvboxLightColorOption1,
+    gruvboxLightColorOption2: gruvboxLightColorOption2,
+    gruvboxLightColorOption3: gruvboxLightColorOption3,
+    gruvboxLightColorOption4: gruvboxLightColorOption4
+});
+
+var gruvboxRedColor$1 = "#f2594b";
+var gruvboxOrangeColor$1 = "#f28534";
+var gruvboxYellowColor$1 = "#e9b143";
+var gruvboxGreenColor$1 = "#b0b846";
+var gruvboxAquaColor$1 = "#8bba7f";
+var gruvboxBlueColor$1 = "#80aa9e";
+var gruvboxPurpleColor$1 = "#d3869b";
+var gruvboxGreyColor$1 = "#928374";
+var gruvboxLightRedColor$1 = "#af2528";
+// const gruvboxLightOrangeColor = '#b94c07'
+var gruvboxLightYellowColor$1 = "#b4730e";
+var gruvboxLightGreenColor$1 = "#72761e";
+var gruvboxLightAquaColor$1 = "#477a5b";
+var gruvboxLightBlueColor$1 = "#266b79";
+var gruvboxLightPurpleColor$1 = "#924f79";
+var gruvboxLightGreyColor$1 = "#7c6f64";
+var gruvboxDarkRedColor$1 = "#b85651";
+// const gruvboxDarkOrangeColor = '#bd6f3e'
+var gruvboxDarkYellowColor$1 = "#c18f41";
+var gruvboxDarkGreenColor$1 = "#8f9a52";
+var gruvboxDarkAquaColor$1 = "#72966c";
+var gruvboxDarkBlueColor$1 = "#68948a";
+var gruvboxDarkPurpleColor$1 = "#ab6c7d";
+var gruvboxDarkGreyColor$1 = "#a89984";
+/* Nothing below has been modified yet. */
+var gruvboxDarkColorSoftContrast$1 = "#32302f";
+var gruvboxDarkColor$1 = "#282828";
+var gruvboxDarkColorHardContrast$1 = "#1d2021";
+var gruvboxDarkColorOption1$1 = "#3c3836";
+var gruvboxDarkColorOption2$1 = "#504945";
+var gruvboxDarkColorOption3$1 = "#665c54";
+var gruvboxDarkColorOption4$1 = "#7c6f64";
+var gruvboxLightColorSoftContrast$1 = "#f2e5bc";
+var gruvboxLightColor$1 = "#fbf1c7";
+var gruvboxLightColorHardContrast$1 = "#f9f5d7";
+var gruvboxLightColorOption1$1 = "#ebdbb2";
+var gruvboxLightColorOption2$1 = "#d5c4a1";
+var gruvboxLightColorOption3$1 = "#bdae93";
+var gruvboxLightColorOption4$1 = "#a89984";
+
+var mixPalette = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    gruvboxRedColor: gruvboxRedColor$1,
+    gruvboxOrangeColor: gruvboxOrangeColor$1,
+    gruvboxYellowColor: gruvboxYellowColor$1,
+    gruvboxGreenColor: gruvboxGreenColor$1,
+    gruvboxAquaColor: gruvboxAquaColor$1,
+    gruvboxBlueColor: gruvboxBlueColor$1,
+    gruvboxPurpleColor: gruvboxPurpleColor$1,
+    gruvboxGreyColor: gruvboxGreyColor$1,
+    gruvboxLightRedColor: gruvboxLightRedColor$1,
+    gruvboxLightYellowColor: gruvboxLightYellowColor$1,
+    gruvboxLightGreenColor: gruvboxLightGreenColor$1,
+    gruvboxLightAquaColor: gruvboxLightAquaColor$1,
+    gruvboxLightBlueColor: gruvboxLightBlueColor$1,
+    gruvboxLightPurpleColor: gruvboxLightPurpleColor$1,
+    gruvboxLightGreyColor: gruvboxLightGreyColor$1,
+    gruvboxDarkRedColor: gruvboxDarkRedColor$1,
+    gruvboxDarkYellowColor: gruvboxDarkYellowColor$1,
+    gruvboxDarkGreenColor: gruvboxDarkGreenColor$1,
+    gruvboxDarkAquaColor: gruvboxDarkAquaColor$1,
+    gruvboxDarkBlueColor: gruvboxDarkBlueColor$1,
+    gruvboxDarkPurpleColor: gruvboxDarkPurpleColor$1,
+    gruvboxDarkGreyColor: gruvboxDarkGreyColor$1,
+    gruvboxDarkColorSoftContrast: gruvboxDarkColorSoftContrast$1,
+    gruvboxDarkColor: gruvboxDarkColor$1,
+    gruvboxDarkColorHardContrast: gruvboxDarkColorHardContrast$1,
+    gruvboxDarkColorOption1: gruvboxDarkColorOption1$1,
+    gruvboxDarkColorOption2: gruvboxDarkColorOption2$1,
+    gruvboxDarkColorOption3: gruvboxDarkColorOption3$1,
+    gruvboxDarkColorOption4: gruvboxDarkColorOption4$1,
+    gruvboxLightColorSoftContrast: gruvboxLightColorSoftContrast$1,
+    gruvboxLightColor: gruvboxLightColor$1,
+    gruvboxLightColorHardContrast: gruvboxLightColorHardContrast$1,
+    gruvboxLightColorOption1: gruvboxLightColorOption1$1,
+    gruvboxLightColorOption2: gruvboxLightColorOption2$1,
+    gruvboxLightColorOption3: gruvboxLightColorOption3$1,
+    gruvboxLightColorOption4: gruvboxLightColorOption4$1
+});
+
+var gruvboxRedColor$2 = "#ea6962";
+var gruvboxOrangeColor$2 = "#e78a4e";
+var gruvboxYellowColor$2 = "#d8a657";
+var gruvboxGreenColor$2 = "#a9b665";
+var gruvboxAquaColor$2 = "#89b482";
+var gruvboxBlueColor$2 = "#7daea3";
+var gruvboxPurpleColor$2 = "#d3869b";
+var gruvboxGreyColor$2 = "#928374";
+var gruvboxLightRedColor$2 = "#c14a4a";
+// const gruvboxLightOrangeColor = '#c35e0a'
+var gruvboxLightYellowColor$2 = "#b47109";
+var gruvboxLightGreenColor$2 = "#6c782e";
+var gruvboxLightAquaColor$2 = "#4c7a5d";
+var gruvboxLightBlueColor$2 = "#45707a";
+var gruvboxLightPurpleColor$2 = "#945e80";
+var gruvboxLightGreyColor$2 = "#7c6f64";
+var gruvboxDarkRedColor$2 = "#b85651";
+// const gruvboxDarkOrangeColor = '#bd6f3e'
+var gruvboxDarkYellowColor$2 = "#c18f41";
+var gruvboxDarkGreenColor$2 = "#8f9a52";
+var gruvboxDarkAquaColor$2 = "#72966c";
+var gruvboxDarkBlueColor$2 = "#68948a";
+var gruvboxDarkPurpleColor$2 = "#ab6c7d";
+var gruvboxDarkGreyColor$2 = "#a89984";
+/* Nothing below has been modified yet. */
+var gruvboxDarkColorSoftContrast$2 = "#32302f";
+var gruvboxDarkColor$2 = "#282828";
+var gruvboxDarkColorHardContrast$2 = "#1d2021";
+var gruvboxDarkColorOption1$2 = "#3c3836";
+var gruvboxDarkColorOption2$2 = "#504945";
+var gruvboxDarkColorOption3$2 = "#665c54";
+var gruvboxDarkColorOption4$2 = "#7c6f64";
+var gruvboxLightColorSoftContrast$2 = "#f2e5bc";
+var gruvboxLightColor$2 = "#fbf1c7";
+var gruvboxLightColorHardContrast$2 = "#f9f5d7";
+var gruvboxLightColorOption1$2 = "#ebdbb2";
+var gruvboxLightColorOption2$2 = "#d5c4a1";
+var gruvboxLightColorOption3$2 = "#bdae93";
+var gruvboxLightColorOption4$2 = "#a89984";
+
+var matPalette = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    gruvboxRedColor: gruvboxRedColor$2,
+    gruvboxOrangeColor: gruvboxOrangeColor$2,
+    gruvboxYellowColor: gruvboxYellowColor$2,
+    gruvboxGreenColor: gruvboxGreenColor$2,
+    gruvboxAquaColor: gruvboxAquaColor$2,
+    gruvboxBlueColor: gruvboxBlueColor$2,
+    gruvboxPurpleColor: gruvboxPurpleColor$2,
+    gruvboxGreyColor: gruvboxGreyColor$2,
+    gruvboxLightRedColor: gruvboxLightRedColor$2,
+    gruvboxLightYellowColor: gruvboxLightYellowColor$2,
+    gruvboxLightGreenColor: gruvboxLightGreenColor$2,
+    gruvboxLightAquaColor: gruvboxLightAquaColor$2,
+    gruvboxLightBlueColor: gruvboxLightBlueColor$2,
+    gruvboxLightPurpleColor: gruvboxLightPurpleColor$2,
+    gruvboxLightGreyColor: gruvboxLightGreyColor$2,
+    gruvboxDarkRedColor: gruvboxDarkRedColor$2,
+    gruvboxDarkYellowColor: gruvboxDarkYellowColor$2,
+    gruvboxDarkGreenColor: gruvboxDarkGreenColor$2,
+    gruvboxDarkAquaColor: gruvboxDarkAquaColor$2,
+    gruvboxDarkBlueColor: gruvboxDarkBlueColor$2,
+    gruvboxDarkPurpleColor: gruvboxDarkPurpleColor$2,
+    gruvboxDarkGreyColor: gruvboxDarkGreyColor$2,
+    gruvboxDarkColorSoftContrast: gruvboxDarkColorSoftContrast$2,
+    gruvboxDarkColor: gruvboxDarkColor$2,
+    gruvboxDarkColorHardContrast: gruvboxDarkColorHardContrast$2,
+    gruvboxDarkColorOption1: gruvboxDarkColorOption1$2,
+    gruvboxDarkColorOption2: gruvboxDarkColorOption2$2,
+    gruvboxDarkColorOption3: gruvboxDarkColorOption3$2,
+    gruvboxDarkColorOption4: gruvboxDarkColorOption4$2,
+    gruvboxLightColorSoftContrast: gruvboxLightColorSoftContrast$2,
+    gruvboxLightColor: gruvboxLightColor$2,
+    gruvboxLightColorHardContrast: gruvboxLightColorHardContrast$2,
+    gruvboxLightColorOption1: gruvboxLightColorOption1$2,
+    gruvboxLightColorOption2: gruvboxLightColorOption2$2,
+    gruvboxLightColorOption3: gruvboxLightColorOption3$2,
+    gruvboxLightColorOption4: gruvboxLightColorOption4$2
+});
+
+var __assign = (undefined && undefined.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __rest = (undefined && undefined.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+var decorateConfig = function (config) {
+    var _a, _b;
+    var _c = config.gruvboxTheme, gruvboxConfig = _c === void 0 ? {} : _c, rest = __rest(config, ["gruvboxTheme"]);
+    var _d = gruvboxConfig.style, themeStyle = _d === void 0 ? "dark" : _d, _e = gruvboxConfig.contrast, themeContrast = _e === void 0 ? "normal" : _e, _f = gruvboxConfig.palette, themePalette = _f === void 0 ? "material" : _f;
+    var p = __rest(themePalette === "original"
+        ? origPalette
+        : themePalette === "mix"
+            ? mixPalette
+            : matPalette, []);
+    var darkThemeTerminalBlack = p.gruvboxDarkColor;
+    var darkThemeTerminalRed = p.gruvboxRedColor;
+    var darkThemeTerminalGreen = p.gruvboxGreenColor;
+    var darkThemeTerminalYellow = p.gruvboxYellowColor;
+    var darkThemeTerminalBlue = p.gruvboxBlueColor;
+    var darkThemeTerminalMagenta = p.gruvboxPurpleColor;
+    var darkThemeTerminalCyan = p.gruvboxAquaColor;
+    var darkThemeTerminalWhite = p.gruvboxLightGreyColor;
+    var darkThemeTerminalBrightBlack = p.gruvboxGreyColor;
+    var darkThemeTerminalBrightRed = p.gruvboxLightRedColor;
+    var darkThemeTerminalBrightGreen = p.gruvboxLightGreenColor;
+    var darkThemeTerminalBrightYellow = p.gruvboxLightYellowColor;
+    var darkThemeTerminalBrightBlue = p.gruvboxLightBlueColor;
+    var darkThemeTerminalBrightMagenta = p.gruvboxLightPurpleColor;
+    var darkThemeTerminalBrightCyan = p.gruvboxLightAquaColor;
+    var darkThemeTerminalBrightWhite = p.gruvboxLightColorOption1;
+    var darkThemeTerminalForeground = p.gruvboxLightColorOption1;
+    var darkThemeTerminalBackgroundSoftContrast = p.gruvboxDarkColorSoftContrast;
+    var darkThemeTerminalBackground = p.gruvboxDarkColor;
+    var darkThemeTerminalBackgroundHardContrast = p.gruvboxDarkColorHardContrast;
+    var lightThemeTerminalBlack = p.gruvboxLightColor;
+    var lightThemeTerminalRed = p.gruvboxRedColor;
+    var lightThemeTerminalGreen = p.gruvboxGreenColor;
+    var lightThemeTerminalYellow = p.gruvboxYellowColor;
+    var lightThemeTerminalBlue = p.gruvboxBlueColor;
+    var lightThemeTerminalMagenta = p.gruvboxPurpleColor;
+    var lightThemeTerminalCyan = p.gruvboxAquaColor;
+    var lightThemeTerminalWhite = p.gruvboxDarkGreyColor;
+    var lightThemeTerminalBrightBlack = p.gruvboxGreyColor;
+    var lightThemeTerminalBrightRed = p.gruvboxDarkRedColor;
+    var lightThemeTerminalBrightGreen = p.gruvboxDarkGreenColor;
+    var lightThemeTerminalBrightYellow = p.gruvboxDarkYellowColor;
+    var lightThemeTerminalBrightBlue = p.gruvboxDarkBlueColor;
+    var lightThemeTerminalBrightMagenta = p.gruvboxDarkPurpleColor;
+    var lightThemeTerminalBrightCyan = p.gruvboxDarkAquaColor;
+    var lightThemeTerminalBrightWhite = p.gruvboxDarkColorOption1;
+    var lightThemeTerminalForeground = p.gruvboxDarkColorOption1;
+    var lightThemeTerminalBackgroundSoftContrast = p.gruvboxLightColorSoftContrast;
+    var lightThemeTerminalBackground = p.gruvboxLightColor;
+    var lightThemeTerminalBackgroundHardContrast = p.gruvboxLightColorHardContrast;
+    var terminalColors = {
+        dark: {
+            black: darkThemeTerminalBlack,
+            red: darkThemeTerminalRed,
+            green: darkThemeTerminalGreen,
+            yellow: darkThemeTerminalYellow,
+            blue: darkThemeTerminalBlue,
+            magenta: darkThemeTerminalMagenta,
+            cyan: darkThemeTerminalCyan,
+            white: darkThemeTerminalWhite,
+            lightBlack: darkThemeTerminalBrightBlack,
+            lightRed: darkThemeTerminalBrightRed,
+            lightGreen: darkThemeTerminalBrightGreen,
+            lightYellow: darkThemeTerminalBrightYellow,
+            lightBlue: darkThemeTerminalBrightBlue,
+            lightMagenta: darkThemeTerminalBrightMagenta,
+            lightCyan: darkThemeTerminalBrightCyan,
+            lightWhite: darkThemeTerminalBrightWhite
+        },
+        light: {
+            black: lightThemeTerminalBlack,
+            red: lightThemeTerminalRed,
+            green: lightThemeTerminalGreen,
+            yellow: lightThemeTerminalYellow,
+            blue: lightThemeTerminalBlue,
+            magenta: lightThemeTerminalMagenta,
+            cyan: lightThemeTerminalCyan,
+            white: lightThemeTerminalWhite,
+            lightBlack: lightThemeTerminalBrightBlack,
+            lightRed: lightThemeTerminalBrightRed,
+            lightGreen: lightThemeTerminalBrightGreen,
+            lightYellow: lightThemeTerminalBrightYellow,
+            lightBlue: lightThemeTerminalBrightBlue,
+            lightMagenta: lightThemeTerminalBrightMagenta,
+            lightCyan: lightThemeTerminalBrightCyan,
+            lightWhite: lightThemeTerminalBrightWhite
+        }
+    };
+    var terminalBackgrounds = {
+        dark: {
+            soft: darkThemeTerminalBackgroundSoftContrast,
+            normal: darkThemeTerminalBackground,
+            hard: darkThemeTerminalBackgroundHardContrast
+        },
+        light: {
+            soft: lightThemeTerminalBackgroundSoftContrast,
+            normal: lightThemeTerminalBackground,
+            hard: lightThemeTerminalBackgroundHardContrast
+        }
+    };
+    var terminalForegrounds = {
+        dark: darkThemeTerminalForeground,
+        light: lightThemeTerminalForeground
+    };
+    var backgroundColorOptions = {
+        dark: {
+            option1: p.gruvboxDarkColorOption1,
+            option2: p.gruvboxDarkColorOption2,
+            option3: p.gruvboxDarkColorOption3,
+            option4: p.gruvboxDarkColorOption4
+        },
+        light: {
+            option1: p.gruvboxLightColorOption1,
+            option2: p.gruvboxLightColorOption2,
+            option3: p.gruvboxLightColorOption3,
+            option4: p.gruvboxLightColorOption4
+        }
+    };
+    var foregroundColor = terminalForegrounds[themeStyle];
+    var backgroundColor = terminalBackgrounds[themeStyle][themeContrast];
+    var backgroundColors = backgroundColorOptions[themeStyle];
+    var backgroundColorForActiveTab = backgroundColors.option1;
+    var cursorMixBlendMode = themeStyle == "dark" ? "lighten" : "darken";
+    return __assign(__assign({}, rest), { foregroundColor: foregroundColor,
+        backgroundColor: backgroundColor, colors: terminalColors[themeStyle], cursorColor: p.gruvboxOrangeColor, css: "\n      " + ((_a = config.css) !== null && _a !== void 0 ? _a : "") + "\n      .tabs_list,\n      .tab_tab,\n      .tabs_borderShim {\n        color: " + foregroundColor + " !important;\n        background-color: " + backgroundColor + " !important;\n        border-color: transparent !important;\n      }\n      .tab_active {\n        background-color: " + backgroundColorForActiveTab + " !important;\n        box-shadow: 2px 0 0 0 " + p.gruvboxOrangeColor + " inset;\n      }\n    ", termCSS: "\n      " + ((_b = config.termCSS) !== null && _b !== void 0 ? _b : "") + "\n      .cursor-node {\n        mix-blend-mode: " + cursorMixBlendMode + ";\n      }\n    " });
+};
+
+exports.decorateConfig = decorateConfig;
---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  input: 'out/index.js',
+  output: {
+    file: 'index.js',
+    format: 'cjs'
+  }
+}
